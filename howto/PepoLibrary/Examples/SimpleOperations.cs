006eabce69ea1d99defe3a40d990b6b584894c71
shirashiki <silvio.hirashiki@gmail.com>
2015-02-03 21:10:48 -0500

    update

---
+++ howto/PepoLibrary/Examples/SimpleOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PepoLibrary.Examples
+{
+    /// <summary>
+    /// A class containing examples of simple operations. DON'T USE THIS AS A LIBRARY.
+    /// This is just to illustrate use, ok?
+    /// 
+    /// See tests in test project, SimpleOperationsTest
+    /// </summary>
+    public class SimpleOperations
+    {
+
+        /// <summary>
+        /// Example: formats number with zeros in the left
+        /// </summary>
+        /// <returns></returns>
+        public static string FormatIntegerWithZerosLeft(int intNumber)
+        {
+            return intNumber.ToString("D8");
+        }
+    }
+}
---
+++ howto/Testing/Unit/SimpleOperationsTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using PepoLibrary.Examples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Unit
+{
+    /// <summary>
+    /// A basic test case, showing how to execute simple operations
+    /// </summary>
+    [TestFixture]
+    public class SimpleOperationsTest
+    {
+        [TestCase]
+        public void FormatIntegerWithZerosLeftTest()
+        {
+            string numFormatted = SimpleOperations.FormatIntegerWithZerosLeft(123);
+            Assert.IsTrue(numFormatted == "00000123");
+        }
+    }
+}
